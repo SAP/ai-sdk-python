702a084e19bceb9e9e1cf520cf7deb351add4458
Marika Marszalkowski <marika.marszalkowski@sap.com>
2026-05-28 14:38:43 +0200

    remove old pipeline files

--- packages/generative-ai-hub-sdk/.pipeline/update_version_custom.sh
+++
@@ -1,6 +0,0 @@
-pip3 install --trusted-host int.repositories.cloud.sap --trusted-host github.wdf.sap.corp --index-url https://int.repositories.cloud.sap/artifactory/api/pypi/build-snapshots-pypi/simple -r requirements.txt
-pydoc3 -w ./
-mv gen_ai_hub*.html docs/
-rm *.html
-echo "------ update version to $1 -----"
-echo "$1" > acceptance_tests/cfg/VERSION
