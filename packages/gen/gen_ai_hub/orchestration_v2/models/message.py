a452d8de52815663374ec2e9ba5b8d6dc170aca6
Sicheng Dong <sicheng.dong@sap.com>
2026-09-15 17:32:11 +0200

    switch AssistantMessage back to BaseModel

--- packages/gen/gen_ai_hub/orchestration_v2/models/message.py
+++ packages/gen/gen_ai_hub/orchestration_v2/models/message.py
@@ -149,7 +149,7 @@ class UserMessage(BaseModel):
         return mapped_content
 
 
-class AssistantMessage(ResponseBaseModel):
+class AssistantMessage(BaseModel):
     """
     Represents an assistant message in a prompt or conversation template.
 
